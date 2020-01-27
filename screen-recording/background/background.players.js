0864ea5c41389c270b7b6e427b5798d5ab0967df
Mohd Azam <azammohd@google.com>
2020-01-27 18:24:34 +0530

    update

---
+++ screen-recording/background/background.players.js
@@ -0,0 +1,9 @@
+var videoPlayers = [];
+
+function initVideoPlayer(stream) {
+    var videoPlayer = document.createElement('video');
+    videoPlayer.muted = !enableTabCaptureAPI;
+    videoPlayer.volume = !!enableTabCaptureAPI;
+    videoPlayer.srcObject = stream;
+    videoPlayers.push(videoPlayer);
+}
